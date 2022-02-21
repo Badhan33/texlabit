8b70cf6050a03b39daef17db582e54af79ec2d8a
Kamrul Hasan <kamrul.ruetcse@gmail.com>
2022-02-21 16:10:31 +0600

    Contact form fix

--- template-parts/contact-us.php
+++ template-parts/contact-us.php
@@ -9,50 +9,28 @@
 
 			<div class="col-lg-6">
 				<div class="map mb-4 mb-lg-0">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3634.3289125304145!2d88.60074231507494!3d24.369858870745873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fbefadd067a1b5%3A0x59b36385bd32bf51!2sTEXLab!5e0!3m2!1sen!2sbd!4v1645203109059!5m2!1sen!2sbd" style="border:0; width: 100%; height: 312px;" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3634.3289125304145!2d88.60074231507494!3d24.369858870745873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fbefadd067a1b5%3A0x59b36385bd32bf51!2sTEXLab!5e0!3m2!1sen!2sbd!4v1645203109059!5m2!1sen!2sbd" style="border:0; width: 100%; height: 450px;" loading="lazy"></iframe>
 				</div>
 			</div>
 
 			<div class="col-lg-6">
 				<div class="row">
-					<div class="col-md-5 info">
+					<div class="col-md-12 info">
 						<i class="ion-ios-location-outline"></i>
 						<p>3rd Floor of Dorjibari, Alokar Mor, Rajshahi, Bangladesh</p>
 					</div>
-					<div class="col-md-4 info">
+					<div class="col-md-12 info">
 						<i class="ion-ios-email-outline"></i>
 						<p>ceo.texlabit@gmail.com</p>
 					</div>
-					<div class="col-md-3 info">
+					<div class="col-md-12 info">
 						<i class="ion-ios-telephone-outline"></i>
 						<p>+880 1752-274447</p>
 					</div>
 				</div>
 
 				<div class="form">
-					<div id="sendmessage">Your message has been sent. Thank you!</div>
-					<div id="errormessage"></div>
-					<form action="" method="post" role="form" class="contactForm">
-						<div class="form-row">
-							<div class="form-group col-lg-6">
-								<input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-								<div class="validation"></div>
-							</div>
-							<div class="form-group col-lg-6">
-								<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-								<div class="validation"></div>
-							</div>
-						</div>
-						<div class="form-group">
-							<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-							<div class="validation"></div>
-						</div>
-						<div class="form-group">
-							<textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-							<div class="validation"></div>
-						</div>
-						<div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
-					</form>
+					<?php echo do_shortcode('[wpforms id="13" title="false"]'); ?>
 				</div>
 			</div>
 
